286809a2470a7e5dd466b621259f36081e4d7e50
sandeep <buthamsandeep123@gmail.com>
2025-08-22 00:11:32 +0530

    frist commit

---
+++ src/test/java/test/Apirun.java
@@ -0,0 +1,74 @@
+package test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class Apirun {
+
+	public static String title;
+	
+	public static void get() 
+	{
+		RestAssured.baseURI="https://jsonplaceholder.typicode.com";
+		
+
+        Response res = 
+        		given()  
+        		.header("Content-Type","Application/Json")
+        		.queryParam("userId","1")
+        		.pathParam("id",1)
+        		.auth().preemptive().basic("username", "password")
+            .when()
+                .get("/posts/{id}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        
+        System.out.println(res.getStatusCode());
+        
+        System.out.println(res.asString());
+     int id=   res.jsonPath().getInt("id");
+   title=  res.jsonPath().getString("title");
+        
+          
+		
+	}
+	
+	public static void post() 
+	{
+		Response res=given()
+		.header("Content-type","Application/json")
+		.header("Authorization","Bearer" + token)
+		.body("\\\"name\\\":\\\"Sandeep\\\", \\\"role\\\":\\\"QA\\\" }")
+		.when()
+		.post("/createusers")
+		.then()
+		.statusCode(201)
+		.extract().response();
+int id=		res.jsonPath().getInt("id");
+String name=		res.jsonPath().get("name");
+		
+		
+	}
+	public static void put() 
+	{
+		
+	}
+	public static void  patach() 
+	{
+		
+	}
+	public static void  delete() 
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		get();
+
+	}
+
+}
---
+++ src/test/java/test/Run.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Run {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://github.com/sandeepbutham/Testing");
+		System.out.println(driver.getTitle());
+		driver.quit();
+
+	}
+
+}
